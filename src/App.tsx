1ab4035ee9d3b9585b1a2a738e94ae185034edac
Shakil53 <shakilahmed.dcc@gmail.com>
2024-10-07 16:32:51 +0600

    implement tally count for every 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment } from "./redux/features/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hooks/hooks";
 
 const App = () => {
-  const {count} = useSelector((state)=> state.counter)
-  const dispatch = useDispatch();
+  const {count} = useAppSelector((state)=> state.counter)
+  const dispatch = useAppDispatch();
   return (
+    <>
     <div className="h-screen w-full flex justify-center items-center">
         <div className="flex border border-purple-300 rounded p-8 bg-gray-50">
         <button onClick={()=> dispatch(increment())} className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white ">Increment</button>
         <h1 className="text-3xl mx-10">{count}</h1>
-        <button onClick={()=> dispatch(decrement())} className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">Decrement</button>
-        </div>
+        <button onClick={() => dispatch(decrement())} className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">Decrement</button>
+        
+      </div>
+       
+      
     </div>
+     <div className="flex ">
+     {Array.from({ length: Math.floor(count / 5) }).map((_, index) => {
+       const boxValue = (index + 1) * 5; // Calculate the value for the box
+       return (
+         <div key={index} className="p-4 border border-blue-300 bg-blue-100 rounded mb-2">
+           Box for count: {boxValue}
+         </div>
+       );
+     })}
+   </div>
+    </>
   );
 };
 
--- src/redux/features/counterSlice.ts
+++ src/redux/features/counterSlice.ts
@@ -1,7 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from '@reduxjs/toolkit'
 
 
-const initialState = {
+type CounterType = {
+    count: number;
+}
+const initialState: CounterType = {
     count: 0
 }
 
@@ -10,15 +14,18 @@ const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
-        increment: (state, action) => {
+        increment: (state) => {
             state.count = state.count + 1;
         },
-        decrement: (state, action) => {
+        incrementByValue: (state, action: PayloadAction<number>) => {
+            state.count = state.count + action.payload.value;
+        },
+        decrement: (state) => {
             state.count = state.count - 1;
         }
     }
 })
 
-export const { increment, decrement } = counterSlice.actions;
+export const { increment, decrement, incrementByValue } = counterSlice.actions;
 
 export default counterSlice.reducer;
